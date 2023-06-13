b2f76978352c968159cdad09ec978290230c2a4c
shyamkryadav <shyamkryadav345@gmail.com>
2023-06-13 05:51:51 +0545

    make a header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  userDetail=[{name:"shyam",email:"shyamkryadv345@gmail.com",phone:"+9779801622236"}]
+  navitem=["Home","Gallery","About","Contact Us"]
+
+}
